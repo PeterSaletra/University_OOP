ebe340ca6198e7665fd4fa6e50e61079bb9271eb
newmon13 <kubalipka123@gmail.com>
2023-12-12 21:33:52 +0100

    Added first version of GUI for Pickly chat application. Commit contains: - Necessary swing components - GUI under MyFrame.java - AI generated logo Added features: - autoscroll to the newest message sent in chat - smooth flow messages - wrapping too long messages

---
+++ Pickly/src/components/RoundBorder.java
@@ -0,0 +1,43 @@
+package components;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.RenderingHints;
+import javax.swing.border.AbstractBorder;
+
+public class RoundBorder extends AbstractBorder {
+    private Color color;
+    private int thickness;
+    private int radius;
+
+    public RoundBorder(Color color, int thickness, int radius) {
+        this.color = color;
+        this.thickness = thickness;
+        this.radius = radius;
+    }
+
+    @Override
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+        super.paintBorder(c, g, x, y, width, height);
+
+        Graphics2D g2d = (Graphics2D) g.create();
+        g2d.setColor(color);
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+        g2d.dispose();
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c) {
+        return new Insets(thickness, thickness, thickness, thickness);
+    }
+
+    @Override
+    public Insets getBorderInsets(Component c, Insets insets) {
+        insets.set(thickness, thickness, thickness, thickness);
+        return insets;
+    }
+}
